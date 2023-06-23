c546885bc9b69e706a0417a89bdef9d23cc7b2cd
hongnguyen0711 <nguyenhongnguyen40@gmail.com>
2023-06-23 17:20:22 +0700

    up io exercise

--- src/ss16_io_file/exercise/read_file/ReadFile.java
+++ src/ss16_io_file/exercise/read_file/ReadFile.java
@@ -1,5 +1,31 @@
 package ss16_io_file.exercise.read_file;
 
-public class ReadFile {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ReadFile {
+    public static List<String> readFile(String pathFile) {
+        List<String> stringList = new ArrayList<>();
+        File file = new File(pathFile);
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                stringList.add(line);
+            }
+            bufferedReader.close();
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringList;
+    }
+
+    public static void main(String[] args) {
+        readFile("src/ss16_io_file/exercise/read_file/readfile");
+    }
 }
